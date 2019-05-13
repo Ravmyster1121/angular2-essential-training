d706d38aae38310952843bcba374e2b7c8158cac
Ravmyster1121 <rhninety@gmail.com>
2019-05-13 18:00:10 +1200

    Followed Tutorial up to ngIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  onMediaItemDelete(mediaItem) {
+    console.log("Deleting media item");
+  }
+
+  firstMediaItem = {
+    id: 1,
+    name: "Firebug",
+    medium: "Series",
+    category: "SciFi",
+    year: 2010,
+    watchedOn: null,
+    isFavorite: false
+  }
+}
